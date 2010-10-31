73bf4adba6f6a986f747915557867559d8e85a24
David Turnbull <dturnbull@gmail.com>
2010-10-31 16:36:05 -0700

    ERB is working.

--- lib/googly/erb.rb
+++ lib/googly/erb.rb
@@ -1,18 +1,41 @@
 class Googly
 
-  # @todo This is a placeholder for ERB support.
+  # Basic support for ERB.
 
   class Erb
     
     include Responses
 
     def initialize(options)
+      @options = options
     end
-    
+
     def call(env)
-      not_found
+      
+      path_info = Rack::Utils.unescape(env["PATH_INFO"])
+
+      return forbidden if path_info.include? ".."
+      
+      template = Errno::ENOENT
+      filename = path_info.gsub(/\.html$/, '.erb')
+      if filename != path_info
+        template = File.read(File.join(@options[:dir], filename)) rescue Errno::ENOENT
+      end
+      if template == Errno::ENOENT
+        template = File.read(File.join(@options[:dir], path_info + '.erb')) rescue Errno::ENOENT
+      end
+      if template == Errno::ENOENT
+        template = File.read(File.join(@options[:dir], path_info + '.html.erb')) rescue Errno::ENOENT
+      end
+      return not_found if template == Errno::ENOENT
+
+      require 'erb'
+      
+      body = ::ERB.new(template).result
+      [200, {"Content-Type" => "text/html",
+         "Content-Length" => body.size.to_s},
+       [body]]
     end
     
   end
-  
 end
---
+++ test/erb_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class ErbTest < Test::Unit::TestCase
+
+  def setup
+    @options = {:dir => File.join(Googly.base_path, 'test')}
+  end
+
+  def test_serve_from_single_file_extension
+    res = Rack::MockRequest.new(Rack::Lint.new(Googly::Erb.new(@options))).
+      get("/erb_test_1.html")
+    assert res.ok?
+    assert res =~ /<html>/
+  end
+
+  def test_serve_from_single_file_extension_with_no_extension
+    res = Rack::MockRequest.new(Rack::Lint.new(Googly::Erb.new(@options))).
+      get("/erb_test_1")
+    assert res.ok?
+    assert res =~ /<html>/
+  end
+
+  def test_serve_from_double_file_extension
+    res = Rack::MockRequest.new(Rack::Lint.new(Googly::Erb.new(@options))).
+      get("/erb_test_2.html")
+    assert res.ok?
+    assert res =~ /<html>/
+  end
+
+  def test_serve_from_double_file_extension_with_no_extension
+    res = Rack::MockRequest.new(Rack::Lint.new(Googly::Erb.new(@options))).
+      get("/erb_test_2")
+    assert res.ok?
+    assert res =~ /<html>/
+  end
+
+  def test_forbidden_response
+    res = Rack::MockRequest.new(Rack::Lint.new(Googly::Erb.new(@options))).
+      get("/../test.html")
+    assert !res.ok?
+    assert res.status == 403
+  end
+end

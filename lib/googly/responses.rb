dc3e35bd5dc672e04658ca3fdb0469572bdc5534
David Turnbull <dturnbull@gmail.com>
2010-11-04 23:25:36 -0700

    whitespace for yardoc

--- lib/googly/responses.rb
+++ lib/googly/responses.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class Googly
   
   # Standard rack responses shared amongst everything Googly.
